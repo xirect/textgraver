dabad94646fd0e3acbc011bc247c60f1425a0492
Jasper de Meijer <Jasperdemeijer@gmail.com>
2017-06-11 22:45:05 +0200

    updated comments

--- Textmining_app/textgraver/entrez_api.py
+++ Textmining_app/textgraver/entrez_api.py
@@ -5,25 +5,30 @@ from Bio import Medline
 
 def pm_ids():
     Entrez.email = "severin@student.han.nl"     # Always tell NCBI who you are
+    #searching pubmed with specific 'terms', entrez will retrieve pubmed id's which will be parsed.
     handle = Entrez.egquery(term="anthocyanin stress")
     record = Entrez.read(handle)
+
     # for row in record["eGQueryResult"]:
     #     if row["DbName"]=="pubmed":
     #          print(row["Count"])
-
+    #searching pubmed with specific 'terms', entrez will retrieve pubmed id's which will be parsed.
     handle = Entrez.esearch(db="pubmed", term="anthocyanin stress", retmax=8000)
+    #parsing of the entrez request
     record = Entrez.read(handle)
     idlist = record["IdList"]
 
+    #medline is used to retrieve an easy readable format of the articles, which will be used for the json format
     handle = Entrez.efetch(db="pubmed", id=idlist, rettype="medline",
                            retmode="text")
+    #parsing the medline request
     records = Medline.parse(handle)
 
     records = list(records)
 
     articles = list()
 
-
+    #creating a json format for storing the article data.
     for record in records:
         pmid = record.get("PMID", "?")
         title = record.get("TI", "?")
@@ -47,5 +52,5 @@ def pm_ids():
         })
 
     articles = json.dumps(articles)
-
+    #returning the list of pubmed id's and the information about the articles in a json format.
     return idlist, articles
